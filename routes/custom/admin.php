32f5fe3109e378eb7e2f889b5e5092e8ef0982b4
norjamillekasan <jamclong0328@gmail.com>
2022-09-14 09:01:33 +0800

    Update admin.php

--- routes/custom/admin.php
+++ routes/custom/admin.php
@@ -1,26 +1,26 @@
 <?php
 
-Route::prefix('admin')->middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-     Route::get('/dashboard', function () {
-        return view('admin-pages.dashboard');
-    })->name('admin.dashboard');
-    Route::get('/manage-rooms', function () {
-        return view('admin-pages.manage-rooms');
-    })->name('admin.manage-rooms');
-    Route::get('/manage-rates', function () {
-        return view('admin-pages.manage-rates');
-    })->name('admin.manage-rates');
-    Route::get('/guests', function () {
-        return view('admin-pages.guests');
-    })->name('admin.guests');
-    Route::get('/manage-users', function () {
-        return view('admin-pages.manage-users');
-    })->name('admin.manage-users');
-});
+// Route::prefix('admin')->middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//      Route::get('/dashboard', function () {
+//         return view('admin-pages.dashboard');
+//     })->name('admin.dashboard');
+//     Route::get('/manage-rooms', function () {
+//         return view('admin-pages.manage-rooms');
+//     })->name('admin.manage-rooms');
+//     Route::get('/manage-rates', function () {
+//         return view('admin-pages.manage-rates');
+//     })->name('admin.manage-rates');
+//     Route::get('/guests', function () {
+//         return view('admin-pages.guests');
+//     })->name('admin.guests');
+//     Route::get('/manage-users', function () {
+//         return view('admin-pages.manage-users');
+//     })->name('admin.manage-users');
+// });
 
 
 // redesign the admin dashboard
